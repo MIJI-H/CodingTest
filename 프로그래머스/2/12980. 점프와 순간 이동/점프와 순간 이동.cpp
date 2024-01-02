07749ee755419355e2f3dad323f283477909e3bf
MIJI-H <71619429+MIJI-H@users.noreply.github.com>
2024-01-02 17:12:09 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 3.91 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int solution(int n)
+{
+    int ans = 0;
+    
+    while(n >= 1){
+        if(n % 2 == 1) ans++;
+        n /= 2;
+    }
+     
+    return ans;
+}
